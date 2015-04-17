cbcf56622a0cbb1361ad8c74af1673daccf31568
ukadev <ukadev@users.noreply.github.com>
2015-04-17 21:15:47 +0100

    Update HomeController.php

--- app/src/Controllers/HomeController.php
+++ app/src/Controllers/HomeController.php
@@ -34,15 +34,17 @@ class HomeController extends Illuminate\Routing\Controller {
 		// Sanitize URL
 		$url = filter_var($_POST['url'], FILTER_SANITIZE_URL);
 		// Check if the original url is online
-		if($this->checkUrl($url) == false){
+		if($this->checkUrl($url) == false)
+		{
 			return json_encode(array('result' => 'error', 'message' => 'Please insert a valid URL'));
 		}
 		// Check if the original URL already exists in the database
 		$exists = Url::whereOriginal(urlencode($url))->first();
 		
-		if(!is_null($exists)){
+		if(!is_null($exists))
+		{
 			$id = $exists['id'];
-		}else{
+		} else {
 			$id = $this->genUniqueCode();
 			// save it into the database
 			$save = new Url;
@@ -88,7 +90,8 @@ class HomeController extends Illuminate\Routing\Controller {
 	public function genUniqueCode($length = 6)
 	{
 		$random = str_random($length);
-		if(Url::find($random)){
+		if(Url::find($random))
+		{
 			$random = $this->genUniqueCode();
 		}
 		return $random;
@@ -101,7 +104,8 @@ class HomeController extends Illuminate\Routing\Controller {
 	*/
 	public function goToLink($id)
 	{
-		if(!is_null($url = Url::whereId($id)->first())){
+		if(!is_null($url = Url::whereId($id)->first()))
+		{
 			Url::whereId($id)->increment('counter');
 			$this->redirect(urldecode($url['original']['original']));
 		}else{
@@ -114,20 +118,22 @@ class HomeController extends Illuminate\Routing\Controller {
 	| Helper function to redirect
 	|--------------------------------------------------------------------------
 	*/
-	function redirect($url = '/notFound') {
-    if(!headers_sent()) {
-        //If headers not sent yet... then do php redirect
-        header('Location: '.$url);
-        exit;
-    } else {
-        //If headers are sent... do javascript redirect... if javascript disabled, do html redirect.
-        echo '<script type="text/javascript">';
-        echo 'window.location.href="'.$url.'";';
-        echo '</script>';
-        echo '<noscript>';
-        echo '<meta http-equiv="refresh" content="0;url='.$url.'" />';
-        echo '</noscript>';
-        exit;
-    }
-}
+	function redirect($url = '/notFound') 
+	{
+	    if(!headers_sent()) 
+	    {
+	        //If headers not sent yet... then do php redirect
+	        header('Location: '.$url);
+	        exit;
+	    } else {
+	        //If headers are sent... do javascript redirect... if javascript disabled, do html redirect.
+	        echo '<script type="text/javascript">';
+	        echo 'window.location.href="'.$url.'";';
+	        echo '</script>';
+	        echo '<noscript>';
+	        echo '<meta http-equiv="refresh" content="0;url='.$url.'" />';
+	        echo '</noscript>';
+	        exit;
+	    }
+	}
 }
